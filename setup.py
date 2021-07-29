368927007d861c92b25720a8e7e99bf31bad9ef6
Jaimeduende27 <88166015+Jaimeduende27@users.noreply.github.com>
2021-07-29 18:01:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_Chopper',
       version='0.0.1',
-      install_requires=['gym','numpy','matplotlib','PIL','random']  # And any other dependencies foo needs
+      install_requires=['gym','numpy','matplotlib','random']  # And any other dependencies foo needs
 )
